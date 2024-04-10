fbf9f267a1a4c406c7db0a52453901910aa74d2b
Alexandre Junior <alejunior.dev@gmail.com>
2024-04-10 10:39:58 -0300

    feat: add facebook authentication feature

--- jest.config.js
+++ jest.config.js
@@ -1,29 +1,28 @@
-// eslint-disable-next-line
 /** @type {import('jest').Config} */
 const config = {
   collectCoverage: false,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: 'coverage',
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "babel",
+  coverageProvider: 'babel',
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "@/tests/(.+)": "<rootDir>/tests/$1",
-    "@/(.+)": "<rootDir>/src/$1",
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
   },
 
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "\\.ts$": "ts-jest",
-  },
-};
+    '\\.ts$': 'ts-jest'
+  }
+}
 
-module.exports = config;
+module.exports = config
