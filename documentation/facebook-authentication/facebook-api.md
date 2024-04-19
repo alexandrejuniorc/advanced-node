5a83a9194cd67d2dcc17eb70ef3bdd339e44f912
Alexandre Junior <alejunior.dev@gmail.com>
2024-04-19 14:39:46 -0300

    feat: ensure FacebookApi gets debug token

--- documentation/facebook-authentication/facebook-api.md
+++ documentation/facebook-authentication/facebook-api.md
@@ -9,14 +9,14 @@
 
 > ## Debug Token
 
-* Request to <https://graph.facebook.com/oauth/debug_token>
+* Request to <https://graph.facebook.com/debug_token>
 * Verb GET
 * Params: access_token (server), input_token (client)
 * Result: { data: { user_id } }
 
 > ## User Info
 
-* Request to <https://graph.facebook.com/oauth/USER_ID>
+* Request to <https://graph.facebook.com/USER_ID>
 * Verb GET
 * Params: fields (id,name,email), access_token (client)
 * Result: { id, name, email }
--- src/infrastructure/apis/facebook.api.ts
+++ src/infrastructure/apis/facebook.api.ts
@@ -8,17 +8,23 @@ export class FacebookApi implements LoadFacebookUserApi {
     private readonly httpClient: HttpGetClient,
     private readonly clientId: string,
     private readonly clientSecret: string
-  ) {}
+  ) { }
 
-  async loadUser (
-    token: LoadFacebookUserApi.Params
-  ): Promise<LoadFacebookUserApi.Result> {
-    await this.httpClient.get({
+  async loadUser (params: LoadFacebookUserApi.Params): Promise<LoadFacebookUserApi.Result> {
+    const appToken = await this.httpClient.get({
       url: `${this.baseUrl}/oauth/access_token`,
       params: {
         client_id: this.clientId,
         client_secret: this.clientSecret,
         grant_type: 'client_credentials'
+      }
+    })
+
+    await this.httpClient.get({
+      url: `${this.baseUrl}/debug_token`,
+      params: {
+        access_token: appToken.access_token,
+        input_token: params.token
       }
     })
 
