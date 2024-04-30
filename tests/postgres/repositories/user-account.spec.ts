5cd71a48cfced3de4b3eaadf9d9bf27c141c79f3
Alexandre Junior <alejunior.dev@gmail.com>
2024-04-29 22:09:03 -0300

    test: ensure PostgresUserAccountRepository returns undefined if email does not exists

--- tests/postgres/repositories/user-account.spec.ts
+++ tests/postgres/repositories/user-account.spec.ts
@@ -51,6 +51,23 @@ describe('PostgresUserAccountRepository', () => {
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('should return undefined if email does not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PostgresUser]
+      })
+      await connection.synchronize()
+      // const postgresUserRepository = getRepository(PostgresUser)
+
+      const sut = new PostgresUserAccountRepository()
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })
